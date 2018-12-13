a11e5824d56df9277531144c082e27e56a16c4f2
Carlos Villavicencio <carlos.po5i@gmail.com>
2018-12-12 23:26:05 -0500

    Preparing workshop stack

--- tech_mornings/demo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.HomeView.as_view()),
-    path('events/', views.EventList.as_view()),
-    path('events/add/', views.EventCreate.as_view(), name='event-add'),
-]
--- tech_mornings/tech_mornings/urls.py
+++ tech_mornings/tech_mornings/urls.py
@@ -19,8 +19,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('demo/', include('demo.urls')),
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Uncomment this to serve /uploads
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
